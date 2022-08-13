cf3d7be6bb92669542b59a92278fe8e39f482311
Sam Karp <sfcypret@gmail.com>
2022-08-13 13:21:51 -0400

    adjusted style of readme to reflect questions in description

--- develop/utilities/generateMarkdown.js
+++ develop/utilities/generateMarkdown.js
@@ -1,11 +1,15 @@
-
+//removed license functions as prompt pulls image of license
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   # Description
+  ### Motivation
   ${data.what}
+  ### Reason for building this application
   ${data.why}
+  ### Problems Solved
   ${data.solve}
+  ### Lessons learned
   ${data.learn}
   ## Installation
   ${data.installation}
